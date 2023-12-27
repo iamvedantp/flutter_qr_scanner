35455339c8a4636f223a6c1e9b628299cb44ebb2
Vedant Pande <vedantp183@gmail.com>
2023-12-27 16:41:04 +0530

    added elevated button in main.dart homepage

--- lib/main.dart
+++ lib/main.dart
@@ -8,98 +8,132 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "QR Code Scanner",
-      theme: ThemeData(primarySwatch: Colors.indigo),
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
       debugShowCheckedModeBanner: false,
-      home: const QRCodeWidget(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Center(
+          child: Text(
+            'QR Code Scanner',
+            style: TextStyle(color: Colors.black87, fontSize: 28),
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const QRCodeWidget(),
+                  ),
+                );
+              },
+              child: const Text(
+                'Scan Now',
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
 class QRCodeWidget extends StatefulWidget {
-  const QRCodeWidget({super.key});
+  const QRCodeWidget({Key? key}) : super(key: key);
 
   @override
   State<QRCodeWidget> createState() => _QRCodeWidgetState();
 }
 
 class _QRCodeWidgetState extends State<QRCodeWidget> {
-  final GlobalKey qrkey = GlobalKey(debugLabel: "QR");
-  QRViewController? controller;
-  String result = " ";
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  late QRViewController controller;
+  String result = '';
 
   @override
   void dispose() {
-    controller?.dispose();
+    controller.dispose();
     super.dispose();
-  }
-
-  void _onQRViewCreated(QRViewController controller) {
-    this.controller = controller;
-    controller.scannedDataStream.listen((scandata) {
-      setState(() {
-        result = scandata.code!;
-      });
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("QR Code Scanner"),
+        title: const Text('QR Code Scanner'),
       ),
       body: Column(
         children: [
           Expanded(
-              flex: 5,
-              child: QRView(
-                key: qrkey,
-                onQRViewCreated: _onQRViewCreated,
-              )),
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: Text(
-                "Scan Result: $result",
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
-              ),
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: (controller) {
+                setState(() {
+                  this.controller = controller;
+                });
+                controller.scannedDataStream.listen((scandata) {
+                  setState(() {
+                    result = scandata.code!;
+                    if (result.isNotEmpty) {
+                      // If result is not empty, close the camera and show options
+                      controller.pauseCamera();
+                    }
+                  });
+                });
+              },
             ),
           ),
-          Expanded(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      if (result.isNotEmpty) {
-                        Clipboard.setData(ClipboardData(text: result));
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text("Copied to Clipboard")),
-                        );
-                      }
-                    },
-                    child: const Text("Copy"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      if (result.isNotEmpty) {
-                        final Uri url = Uri.parse(result);
-                        await launchUrl(url);
-                      }
-                    },
-                    child: const Text("Open"),
-                  ),
-                ],
-              ))
+          if (result.isNotEmpty)
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Clipboard.setData(ClipboardData(text: result));
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Copied to Clipboard'),
+                      ),
+                    );
+                  },
+                  child: const Text('Copy'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    final Uri url = Uri.parse(result);
+                    launchUrl(url);
+                  },
+                  child: const Text('Open'),
+                ),
+              ],
+            ),
         ],
       ),
     );
